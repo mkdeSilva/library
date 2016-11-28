86154c779fe07d1aa328a2dfe99348926fab962e
Pavel Farhan <Pavel Farhan>
2016-11-28 13:21:18 +0700

    weird shit going on with index.php | fixed it

--- index.php
+++ index.php
@@ -17,13 +17,7 @@
 <body>
 	<center>
 	<header><?php require_once('header.php'); ?></header>
-<<<<<<< HEAD
 	<img src="pictures/InovaAntaresLogo2.jpg" style="width: 80%; height:auto" />
-=======
-	<img src="pictures/InovaAntaresLogo2.jpg" style="width: 100%; height:auto" />
-    <h2><span class="flatInvertedLink"><a href="login.php">Please click here to log in to the system.</a></span></h2>
->>>>>>> 295ba377f895dd44c66c3f5ad9fd92ab53b5a489
-
     </center>
 </body>
     
