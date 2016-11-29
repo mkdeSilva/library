2abe53d340cb3f5e2afba4431f20536de8259ce3
Pavel Farhan <Pavel Farhan>
2016-11-29 20:36:10 +0700

    Added returnBook.php but no edits made

---
+++ returnBook.php
@@ -0,0 +1,20 @@
+<?php
+require_once('connect.php');
+session_start();
+$editID = $_POST['bookID'];
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Renting Book</title>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="design.css">
+</head>
+<body>
+	<header><?php require_once('header.php') ?> </header>
+	<br><br><br><br><br>
+	<br><br>
+
+
+</body>
+</html>
