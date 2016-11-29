eb90fbf53814a11dab60b15949ecc20b6dba1948
mkdesilva <mihindu.desilva@gmail.com>
2016-11-29 16:43:41 +0700

    Added updated library database, please import

--- library.sql
+++ library.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Nov 28, 2016 at 05:54 AM
+-- Generation Time: Nov 29, 2016 at 10:42 AM
 -- Server version: 5.6.33
 -- PHP Version: 7.0.12
 
@@ -57,14 +57,76 @@ CREATE TABLE `book` (
 --
 
 INSERT INTO `book` (`bookID`, `description`, `bookName`, `bookAuthor`, `bookPrice`, `bookGenre`, `pubDate`, `stock`, `imageLink`) VALUES
-(1, 'A young man is destined to be a dragon rider', 'Eragon', 'Christopher Paolini', 50, 'Fantasy', '2001-11-01', 12, 'pictures/eragon.jpg'),
+(1, 'A young man is destined to be a dragon rider', 'Eragon', 'Christopher Paolini', 50, 'Fantasy', '2001-11-01', 0, 'pictures/eragon.jpg'),
 (2, 'A demigod that must learn about his destiny', 'Percy Jackson: The Lightning Thief', 'Rick Riordan', 250, 'Fantasy', '2010-11-22', 3, 'http://vignette3.wikia.nocookie.net/olympians/images/1/15/LightningThief2014.jpg/revision/latest?cb=20140218183347'),
 (3, 'Percy Jackson goes on another journey', 'Percy Jackson: The Sea of Monsters', 'Rick Riordan', 499, 'Fantasy', '2006-05-01', 2, 'https://images-na.ssl-images-amazon.com/images/I/51bmcaOTUaL._SY344_BO1,204,203,200_.jpg'),
 (4, 'Eragon must join the Varden and Ronan must journey south', 'Eldest', 'Christopher Paolini', 400, 'Fantasy', '2005-08-23', 5, 'https://upload.wikimedia.org/wikipedia/en/e/e0/Eldest_book_cover.png'),
 (5, 'Eragon must visit the elves and forge his true sword', 'Brisingr', 'Christopher Paolini', 400, 'Fantasy', '2008-09-02', 5, 'https://upload.wikimedia.org/wikipedia/en/7/70/Brisingr_book_cover.png'),
-(6, 'Galbatorix is preparing and Eragon must face him', 'Inheritance', 'Christopher Paolini', 500, 'Fantasy', '2011-11-08', 99, 'https://upload.wikimedia.org/wikipedia/en/2/2b/Inheritance2011.JPG'),
-(9, 'Who is the person that opens the chamber of secrets?', 'Harry Potter and the Chamber of Secrets', 'J.K. Rowling', 400, 'Fantasy', '1998-07-02', 43, 'https://upload.wikimedia.org/wikipedia/en/a/a7/Harry_Potter_and_the_Chamber_of_Secrets_(US_cover).jpg'),
-(10, 'The story of Sonea, a young girl from the slums, as she discovers her magical potential', 'The Magician\'s Guild', 'Trudi Canavan', 200, 'Fantasy', '2001-09-02', 29, 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e7/TrudiCanavan_TheMagiciansGuild.jpg/220px-TrudiCanavan_TheMagiciansGuild.jpg');
+(6, 'Galbatorix is preparing and Eragon must face him', 'Inheritance', 'Christopher Paolini', 500, 'Fantasy', '2011-11-08', 6, 'https://upload.wikimedia.org/wikipedia/en/2/2b/Inheritance2011.JPG'),
+(9, 'Who is the person that opens the chamber of secrets?', 'Harry Potter and the Chamber of Secrets', 'J.K. Rowling', 400, 'Fantasy', '1998-07-02', 7, 'https://upload.wikimedia.org/wikipedia/en/a/a7/Harry_Potter_and_the_Chamber_of_Secrets_(US_cover).jpg'),
+(10, 'The story of Sonea, a young girl from the slums, as she discovers her magical potential', 'The Magician\'s Guild', 'Trudi Canavan', 200, 'Fantasy', '2001-09-02', 2, 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e7/TrudiCanavan_TheMagiciansGuild.jpg/220px-TrudiCanavan_TheMagiciansGuild.jpg'),
+(13, 'The story follows Harry Potter, a wizard in his fourth year at Hogwarts School of Witchcraft and Wizardry and the mystery surrounding the entry of Harry\'s name into the Triwizard Tournament, in which he is forced to compete.', 'Harry Potter and the Goblet of Fire', 'J.K. Rowling', 500, 'Fantasy', '2008-09-12', 5, 'https://upload.wikimedia.org/wikipedia/en/c/c7/Harry_Potter_and_the_Goblet_of_Fire.jpg'),
+(14, 'The story follows Harry Potter, a wizard in his fourth year at Hogwarts School of Witchcraft and Wizardry and the mystery surrounding the entry of Harry\'s name into the Triwizard Tournament, in which he is forced to compete.', 'Harry Potter and the Goblet of Fire', 'J.K. Rowling', 500, 'Fantasy', '2008-09-12', 5, 'https://upload.wikimedia.org/wikipedia/en/c/c7/Harry_Potter_and_the_Goblet_of_Fire.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookCopies`
+--
+
+CREATE TABLE `bookCopies` (
+  `bookCopyID` int(11) NOT NULL,
+  `bookID` int(11) NOT NULL,
+  `available` int(11) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `bookCopies`
+--
+
+INSERT INTO `bookCopies` (`bookCopyID`, `bookID`, `available`) VALUES
+(1, 1, 0),
+(43, 1, 0),
+(84, 2, 1),
+(85, 2, 1),
+(86, 2, 1),
+(87, 3, 1),
+(88, 3, 1),
+(89, 4, 1),
+(90, 4, 1),
+(91, 4, 1),
+(92, 4, 1),
+(93, 4, 1),
+(94, 5, 1),
+(95, 5, 1),
+(96, 5, 1),
+(97, 5, 1),
+(98, 5, 1),
+(99, 6, 1),
+(100, 6, 1),
+(101, 6, 1),
+(102, 6, 1),
+(103, 6, 1),
+(104, 6, 1),
+(105, 9, 1),
+(106, 9, 1),
+(107, 9, 1),
+(108, 9, 1),
+(109, 9, 1),
+(110, 9, 1),
+(111, 9, 1),
+(112, 10, 1),
+(113, 10, 1),
+(114, 13, 1),
+(115, 13, 1),
+(116, 13, 1),
+(117, 13, 1),
+(118, 13, 1),
+(119, 14, 1),
+(120, 14, 1),
+(121, 14, 1),
+(122, 14, 1),
+(123, 14, 1);
 
 -- --------------------------------------------------------
 
@@ -135,9 +197,9 @@ CREATE TABLE `rentdetails` (
   `dateOfIssue` date NOT NULL,
   `dateOfReturn` date NOT NULL,
   `deposit` float NOT NULL,
-  `bookID` int(11) NOT NULL,
-  `staffID` int(11) NOT NULL,
-  `studentID` int(11) NOT NULL
+  `bookCopyID` int(11) NOT NULL,
+  `studentID` int(11) NOT NULL,
+  `active` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -213,6 +275,13 @@ ALTER TABLE `book`
   ADD PRIMARY KEY (`bookID`);
 
 --
+-- Indexes for table `bookCopies`
+--
+ALTER TABLE `bookCopies`
+  ADD PRIMARY KEY (`bookCopyID`),
+  ADD KEY `bookID` (`bookID`);
+
+--
 -- Indexes for table `jobs`
 --
 ALTER TABLE `jobs`
@@ -238,8 +307,7 @@ ALTER TABLE `purchaseorders`
 --
 ALTER TABLE `rentdetails`
   ADD PRIMARY KEY (`rentID`),
-  ADD KEY `bookID` (`bookID`),
-  ADD KEY `staffID` (`staffID`),
+  ADD KEY `bookID` (`bookCopyID`),
   ADD KEY `studentID` (`studentID`);
 
 --
@@ -268,7 +336,12 @@ ALTER TABLE `authors`
 -- AUTO_INCREMENT for table `book`
 --
 ALTER TABLE `book`
-  MODIFY `bookID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `bookID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+--
+-- AUTO_INCREMENT for table `bookCopies`
+--
+ALTER TABLE `bookCopies`
+  MODIFY `bookCopyID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=124;
 --
 -- AUTO_INCREMENT for table `jobs`
 --
@@ -288,7 +361,7 @@ ALTER TABLE `purchaseorders`
 -- AUTO_INCREMENT for table `rentdetails`
 --
 ALTER TABLE `rentdetails`
-  MODIFY `rentID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `rentID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `staff`
 --
@@ -315,6 +388,5 @@ ALTER TABLE `purchaseorders`
 -- Constraints for table `rentdetails`
 --
 ALTER TABLE `rentdetails`
-  ADD CONSTRAINT `rent_book_fk` FOREIGN KEY (`bookID`) REFERENCES `book` (`bookID`),
-  ADD CONSTRAINT `rent_staff_fk` FOREIGN KEY (`staffID`) REFERENCES `staff` (`staffID`),
+  ADD CONSTRAINT `rent_book_fk` FOREIGN KEY (`bookCopyID`) REFERENCES `book` (`bookID`),
   ADD CONSTRAINT `rent_student_fk` FOREIGN KEY (`studentID`) REFERENCES `students` (`studentID`);
