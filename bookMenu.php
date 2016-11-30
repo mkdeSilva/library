43e3a73f6c4f137ac9f46d83a263bfabe3150444
mkdesilva <mihindu.desilva@gmail.com>
2016-11-30 09:03:46 +0700

    Updated database

--- bookMenu.php
+++ bookMenu.php
@@ -31,136 +31,136 @@ require_once('connect.php'); //dont’ know if I need this, check once this page
 					<ul>
 						<li><a href='bookMenu.php'><h2><span class="active">Books</span> | </h2></a></li>
 						<li><a href='publisherMenu.php'><h2><span>Publishers</span> | </h2></a></li>
-						<li><a href='studentMenu.php'><h2><span>View Students</span></h2></a></li>
-					</ul>
-				</div>
-			</center><br><br><br><br>
-
-			<center>
-				<div style="color: white;padding: 30px;">
-					<h2><span id="addBooksButton" class="flatChoiceActive"><a>Add a book</a></span>|<span id="viewBooksButton" class="flatChoiceInactive"><a>View Books</a></span></h2>
-				</div>
-			</center>
-
-			<form id="addBookForm" action="addBook.php" method="POST">
-				<div id="allInputs">
-
-					<!-- ADD BOOK FORM -->
-
-					<div style="float:left">
-						<label>name</label><br>
-						<input type="text" name="bookName" class="flatInput">
-						<br><br>
-						<label>author</label><br>
-						<input type="text" name="bookAuthor" class="flatInput">
-						<br><br>
-						<label>description</label><br>
-						<textarea name="bookDescription" class="flatLargeInput"></textarea>
-						<br><br>
-						<label>image</label><br>
-						<input type="text" name="bookImage" class="flatInput" placeholder="insert link to image">
-						<br><br>
+						<lia><a href='studentMenu.php'><h2><span>View Students</span></h2></a></li>
+						</ul>
 					</div>
-					<div style="float:right">
-						<label>price</label><br>
-						<input type="text" name="bookPrice" class="flatInput">
-						<br><br>
-						<label>genre</label><br>
-						<input type="text" name="bookGenre" class="flatInput">
-						<br><br>
-						<label>publication date (yyyy/mm/dd)</label><br>
-						<input type="date" name="bookDate" class="flatInput" placeholder="1999-12-01"> <!-- ------- -->
-						<br><br>
-						<label>stock</label><br>
-						<input type="text" name="bookStock" class="flatInput">
-						<br><br>
-						<br>
-						<input type="submit" value="Add Book" class="flatButton">	
+				</center><br><br><br><br>
+
+				<center>
+					<div style="color: white;padding: 30px;">
+						<h2><span id="addBooksButton" class="flatChoiceActive"><a>Add a book</a></span>|<span id="viewBooksButton" class="flatChoiceInactive"><a>View Books</a></span></h2>
 					</div>
-				</div>
-			</form>
+				</center>
 
-			<!-- VIEW BOOKS-->
+				<form id="addBookForm" action="addBook.php" method="POST">
+					<div id="allInputs">
 
-			<div style="display:none" id="viewBookForm">
+						<!-- ADD BOOK FORM -->
 
-				<table>
-					<?php
-					$q="SELECT * FROM book;";
-					$q = strtolower($q);
-					$result=$mysqli->query($q);
-					if(!$result){
-						echo "Select failed. Error: ".$mysqli->error ;
-						break;
-					}else{
-						?>
-						<tr>
-						<th>Name</th>
-							<th>Author</th>
-							<th>Description</th>
-							<th>Price</th>
-							<th>Genre</th>
-							<th>Publication Date</th>
-							<th>Stock</th>
-							<th>Image Link</th>
-							<th>Edit</th>
-							<th>Delete</th>
-						</tr>
+						<div style="float:left">
+							<label>name</label><br>
+							<input type="text" name="bookName" class="flatInput">
+							<br><br>
+							<label>author</label><br>
+							<input type="text" name="bookAuthor" class="flatInput">
+							<br><br>
+							<label>description</label><br>
+							<textarea name="bookDescription" class="flatLargeInput"></textarea>
+							<br><br>
+							<label>image</label><br>
+							<input type="text" name="bookImage" class="flatInput" placeholder="insert link to image">
+							<br><br>
+						</div>
+						<div style="float:right">
+							<label>price</label><br>
+							<input type="text" name="bookPrice" class="flatInput">
+							<br><br>
+							<label>genre</label><br>
+							<input type="text" name="bookGenre" class="flatInput">
+							<br><br>
+							<label>publication date (yyyy/mm/dd)</label><br>
+							<input type="date" name="bookDate" class="flatInput" placeholder="1999-12-01"> <!-- ------- -->
+							<br><br>
+							<label>stock</label><br>
+							<input type="text" name="bookStock" class="flatInput">
+							<br><br>
+							<br>
+							<input type="submit" value="Add Book" class="flatButton">	
+						</div>
+					</div>
+				</form>
+
+				<!-- VIEW BOOKS-->
+
+				<div style="display:none" id="viewBookForm">
+
+					<table>
 						<?php
+						$q="SELECT * FROM book;";
+						$q = strtolower($q);
+						$result=$mysqli->query($q);
+						if(!$result){
+							echo "Select failed. Error: ".$mysqli->error ;
+							break;
+						}else{
+							?>
+							<tr>
+								<th>Name</th>
+								<th>Author</th>
+								<th>Description</th>
+								<th>Price</th>
+								<th>Genre</th>
+								<th>Publication Date</th>
+								<th>Stock</th>
+								<th>Image Link</th>
+								<th>Edit</th>
+								<th>Delete</th>
+							</tr>
+							<?php
 
-						while($row=$result->fetch_array()){ ?>
-						<tr>
-							<td><?=$row['bookName']?></td> 
-							<td><?=$row['bookAuthor']?></td>
-							<td><?=$row['description']?></td>
-							<td><?=$row['bookPrice']?></td>
-							<td><?=$row['bookGenre']?></td>
-							<td><?=$row['pubDate']?></td>
-							<td><?=$row['stock']?></td>
-							<td> <img height=100px src="<?=$row['imageLink']?>"></td>
-							<td align="center" valign="middle">
-								<a href="editBook.php?bookID=<?=$row['bookID']?>"><img src="pictures/edit.ico" width="24" height="24"></a>
-							</td>
-							<td align="center" valign="middle">
-								<a href='deleteBook.php?bookID=<?=$row['bookID']?>'> <img src="pictures/delete.ico" width="24" height="24"></a></td>
-						</tr>                               
-						<?php }} ?>
-					</table>
+							while($row=$result->fetch_array()){ ?>
+							<tr>
+								<td><?=$row['bookName']?></td> 
+								<td><?=$row['bookAuthor']?></td>
+								<td><?=$row['description']?></td>
+								<td><?=$row['bookPrice']?></td>
+								<td><?=$row['bookGenre']?></td>
+								<td><?=$row['pubDate']?></td>
+								<td><?=$row['stock']?></td>
+								<td> <img height=100px src="<?=$row['imageLink']?>"></td>
+								<td align="center" valign="middle">
+									<a href="editBook.php?bookID=<?=$row['bookID']?>"><img src="pictures/edit.ico" width="24" height="24"></a>
+								</td>
+								<td align="center" valign="middle">
+									<a href='deleteBook.php?bookID=<?=$row['bookID']?>'> <img src="pictures/delete.ico" width="24" height="24"></a></td>
+								</tr>                               
+								<?php }} ?>
+							</table>
 
-				</div>
-
-
-				<?php 
-			}else if($_SESSION['permission'] == 'student'){
-				echo "<br><br><br><br><center><h2>You are a student, go here: <span class='flatLink'><a href='studentHome.php'>Student Home</a></span></h2></center>";
-			}
-		}else{
-			echo "<br><br><br><br><center><h2>You must login to see the contents of our website: <span class='flatLink'><a href = 'login.php'>Login</a></span></h2></center>";
-		}
-		?>
+						</div>
 
 
-		<script src='jQuery.js'></script>
-		<script>
-			$(function(){
-				$("#addBooksButton").click(function(){
-					$("#viewBookForm").hide();
-					$("#addBookForm").show();
-					$(this).addClass('flatChoiceActive').removeClass('flatChoiceInactive');
-					$("#viewBooksButton").addClass('flatChoiceInactive').removeClass('flatChoiceActive');
-				});
-			});
+						<?php 
+					}else if($_SESSION['permission'] == 'student'){
+						echo "<br><br><br><br><center><h2>You are a student, go here: <span class='flatLink'><a href='studentHome.php'>Student Home</a></span></h2></center>";
+					}
+				}else{
+					echo "<br><br><br><br><center><h2>You must login to see the contents of our website: <span class='flatLink'><a href = 'login.php'>Login</a></span></h2></center>";
+				}
+				?>
 
-			$(function(){
-				$("#viewBooksButton").click(function(){
-					$("#addBookForm").hide();
-					$("#viewBookForm").show();
-					$(this).addClass('flatChoiceActive').removeClass('flatChoiceInactive');
-					$("#addBooksButton").addClass('flatChoiceInactive').removeClass('flatChoiceActive');
-				});
-			});
-		</script>
 
-	</body>
+				<script src='jQuery.js'></script>
+				<script>
+					$(function(){
+						$("#addBooksButton").click(function(){
+							$("#viewBookForm").hide();
+							$("#addBookForm").show();
+							$(this).addClass('flatChoiceActive').removeClass('flatChoiceInactive');
+							$("#viewBooksButton").addClass('flatChoiceInactive').removeClass('flatChoiceActive');
+						});
+					});
 
-	</html
+					$(function(){
+						$("#viewBooksButton").click(function(){
+							$("#addBookForm").hide();
+							$("#viewBookForm").show();
+							$(this).addClass('flatChoiceActive').removeClass('flatChoiceInactive');
+							$("#addBooksButton").addClass('flatChoiceInactive').removeClass('flatChoiceActive');
+						});
+					});
+				</script>
+
+			</body>
+
+			</html
--- library.sql
+++ library.sql
@@ -1,20 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 29, 2016 at 11:45 AM
--- Server version: 10.1.16-MariaDB
--- PHP Version: 5.6.24
+-- Host: localhost:3306
+-- Generation Time: Nov 30, 2016 at 03:03 AM
+-- Server version: 5.6.33
+-- PHP Version: 7.0.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `library`
@@ -29,16 +23,15 @@ SET time_zone = "+00:00";
 CREATE TABLE `authors` (
   `authorID` int(11) NOT NULL,
   `authorName` varchar(255) NOT NULL,
-  `website` varchar(255) NOT NULL,
-  `publisher` int(11) NOT NULL
+  `website` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `authors`
 --
 
-INSERT INTO `authors` (`authorID`, `authorName`, `website`, `publisher`) VALUES
-(1, 'Alex Dally', 'aDally@gmail.com', 1);
+INSERT INTO `authors` (`authorID`, `authorName`, `website`) VALUES
+(1, 'Alex Dally', 'aDally@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -55,23 +48,24 @@ CREATE TABLE `book` (
   `bookGenre` varchar(45) NOT NULL,
   `pubDate` date NOT NULL,
   `stock` int(11) NOT NULL,
-  `imageLink` varchar(255) NOT NULL
+  `imageLink` varchar(255) NOT NULL,
+  `authorID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `book`
 --
 
-INSERT INTO `book` (`bookID`, `description`, `bookName`, `bookAuthor`, `bookPrice`, `bookGenre`, `pubDate`, `stock`, `imageLink`) VALUES
-(1, 'A young man is destined to be a dragon rider', 'Eragon', 'Christopher Paolini', 50, 'Fantasy', '2001-11-01', 2, 'pictures/eragon.jpg'),
-(2, 'A demigod that must learn about his destiny', 'Percy Jackson: The Lightning Thief', 'Rick Riordan', 250, 'Fantasy', '2010-11-22', 2, 'http://vignette3.wikia.nocookie.net/olympians/images/1/15/LightningThief2014.jpg/revision/latest?cb=20140218183347'),
-(3, 'Percy Jackson goes on another journey', 'Percy Jackson: The Sea of Monsters', 'Rick Riordan', 499, 'Fantasy', '2006-05-01', 2, 'https://images-na.ssl-images-amazon.com/images/I/51bmcaOTUaL._SY344_BO1,204,203,200_.jpg'),
-(4, 'Eragon must join the Varden and Ronan must journey south', 'Eldest', 'Christopher Paolini', 400, 'Fantasy', '2005-08-23', 5, 'https://upload.wikimedia.org/wikipedia/en/e/e0/Eldest_book_cover.png'),
-(5, 'Eragon must visit the elves and forge his true sword', 'Brisingr', 'Christopher Paolini', 400, 'Fantasy', '2008-09-02', 4, 'https://upload.wikimedia.org/wikipedia/en/7/70/Brisingr_book_cover.png'),
-(6, 'Galbatorix is preparing and Eragon must face him', 'Inheritance', 'Christopher Paolini', 500, 'Fantasy', '2011-11-08', 4, 'https://upload.wikimedia.org/wikipedia/en/2/2b/Inheritance2011.JPG'),
-(9, 'Who is the person that opens the chamber of secrets?', 'Harry Potter and the Chamber of Secrets', 'J.K. Rowling', 400, 'Fantasy', '1998-07-02', 6, 'https://upload.wikimedia.org/wikipedia/en/a/a7/Harry_Potter_and_the_Chamber_of_Secrets_(US_cover).jpg'),
-(10, 'The story of Sonea, a young girl from the slums, as she discovers her magical potential', 'The Magician''s Guild', 'Trudi Canavan', 200, 'Fantasy', '2001-09-02', 1, 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e7/TrudiCanavan_TheMagiciansGuild.jpg/220px-TrudiCanavan_TheMagiciansGuild.jpg'),
-(13, 'The story follows Harry Potter, a wizard in his fourth year at Hogwarts School of Witchcraft and Wizardry and the mystery surrounding the entry of Harry''s name into the Triwizard Tournament, in which he is forced to compete.', 'Harry Potter and the Goblet of Fire', 'J.K. Rowling', 500, 'Fantasy', '2008-09-12', 5, 'https://upload.wikimedia.org/wikipedia/en/c/c7/Harry_Potter_and_the_Goblet_of_Fire.jpg');
+INSERT INTO `book` (`bookID`, `description`, `bookName`, `bookAuthor`, `bookPrice`, `bookGenre`, `pubDate`, `stock`, `imageLink`, `authorID`) VALUES
+(1, 'A young man is destined to be a dragon rider', 'Eragon', 'Christopher Paolini', 50, 'Fantasy', '2001-11-01', 2, 'pictures/eragon.jpg', 0),
+(2, 'A demigod that must learn about his destiny', 'Percy Jackson: The Lightning Thief', 'Rick Riordan', 250, 'Fantasy', '2010-11-22', 2, 'http://vignette3.wikia.nocookie.net/olympians/images/1/15/LightningThief2014.jpg/revision/latest?cb=20140218183347', 0),
+(3, 'Percy Jackson goes on another journey', 'Percy Jackson: The Sea of Monsters', 'Rick Riordan', 499, 'Fantasy', '2006-05-01', 2, 'https://images-na.ssl-images-amazon.com/images/I/51bmcaOTUaL._SY344_BO1,204,203,200_.jpg', 0),
+(4, 'Eragon must join the Varden and Ronan must journey south', 'Eldest', 'Christopher Paolini', 400, 'Fantasy', '2005-08-23', 4, 'https://upload.wikimedia.org/wikipedia/en/e/e0/Eldest_book_cover.png', 0),
+(5, 'Eragon must visit the elves and forge his true sword', 'Brisingr', 'Christopher Paolini', 400, 'Fantasy', '2008-09-02', 4, 'https://upload.wikimedia.org/wikipedia/en/7/70/Brisingr_book_cover.png', 0),
+(6, 'Galbatorix is preparing and Eragon must face him', 'Inheritance', 'Christopher Paolini', 500, 'Fantasy', '2011-11-08', 4, 'https://upload.wikimedia.org/wikipedia/en/2/2b/Inheritance2011.JPG', 0),
+(9, 'Who is the person that opens the chamber of secrets?', 'Harry Potter and the Chamber of Secrets', 'J.K. Rowling', 400, 'Fantasy', '1998-07-02', 6, 'https://upload.wikimedia.org/wikipedia/en/a/a7/Harry_Potter_and_the_Chamber_of_Secrets_(US_cover).jpg', 0),
+(10, 'The story of Sonea, a young girl from the slums, as she discovers her magical potential', 'The Magician\'s Guild', 'Trudi Canavan', 200, 'Fantasy', '2001-09-02', 1, 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e7/TrudiCanavan_TheMagiciansGuild.jpg/220px-TrudiCanavan_TheMagiciansGuild.jpg', 0),
+(13, 'The story follows Harry Potter, a wizard in his fourth year at Hogwarts School of Witchcraft and Wizardry and the mystery surrounding the entry of Harry\'s name into the Triwizard Tournament, in which he is forced to compete.', 'Harry Potter and the Goblet of Fire', 'J.K. Rowling', 500, 'Fantasy', '2008-09-12', 5, 'https://upload.wikimedia.org/wikipedia/en/c/c7/Harry_Potter_and_the_Goblet_of_Fire.jpg', 0);
 
 -- --------------------------------------------------------
 
@@ -95,37 +89,37 @@ INSERT INTO `bookcopies` (`bookCopyID`, `bookID`, `available`) VALUES
 (129, 5, 0),
 (153, 2, 0),
 (209, 10, 0),
-(216, 1, 1),
-(217, 1, 1),
-(218, 2, 1),
-(219, 2, 1),
-(220, 3, 1),
-(221, 3, 1),
-(222, 4, 1),
-(223, 4, 1),
-(224, 4, 1),
-(225, 4, 1),
-(226, 4, 1),
-(227, 5, 1),
-(228, 5, 1),
-(229, 5, 1),
-(230, 5, 1),
-(231, 6, 1),
-(232, 6, 1),
-(233, 6, 1),
-(234, 6, 1),
-(235, 9, 1),
-(236, 9, 1),
-(237, 9, 1),
-(238, 9, 1),
-(239, 9, 1),
-(240, 9, 1),
-(241, 10, 1),
-(242, 13, 1),
-(243, 13, 1),
-(244, 13, 1),
-(245, 13, 1),
-(246, 13, 1);
+(222, 4, 0),
+(247, 1, 1),
+(248, 1, 1),
+(249, 2, 1),
+(250, 2, 1),
+(251, 3, 1),
+(252, 3, 1),
+(253, 4, 1),
+(254, 4, 1),
+(255, 4, 1),
+(256, 4, 1),
+(257, 5, 1),
+(258, 5, 1),
+(259, 5, 1),
+(260, 5, 1),
+(261, 6, 1),
+(262, 6, 1),
+(263, 6, 1),
+(264, 6, 1),
+(265, 9, 1),
+(266, 9, 1),
+(267, 9, 1),
+(268, 9, 1),
+(269, 9, 1),
+(270, 9, 1),
+(271, 10, 1),
+(272, 13, 1),
+(273, 13, 1),
+(274, 13, 1),
+(275, 13, 1),
+(276, 13, 1);
 
 -- --------------------------------------------------------
 
@@ -154,40 +148,6 @@ INSERT INTO `jobs` (`jobID`, `jobTitle`, `jobDescription`, `jobSalary`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `publishers`
---
-
-CREATE TABLE `publishers` (
-  `pubID` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `location` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `publishers`
---
-
-INSERT INTO `publishers` (`pubID`, `name`, `location`) VALUES
-(2, 'Scholastic', 'United States of America');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `purchaseorders`
---
-
-CREATE TABLE `purchaseorders` (
-  `orderID` int(11) NOT NULL,
-  `pricePerBook` float NOT NULL,
-  `quantity` int(11) NOT NULL,
-  `bookISBN` int(11) NOT NULL,
-  `publisher` int(11) NOT NULL,
-  `staffMember` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `rentdetails`
 --
 
@@ -210,7 +170,8 @@ INSERT INTO `rentdetails` (`rentID`, `dateOfIssue`, `dateOfReturn`, `deposit`, `
 (13, '2016-11-29', '2016-12-06', 40, 103, 14, 1),
 (14, '2016-11-29', '2016-12-06', 40, 129, 1, 1),
 (15, '2016-11-29', '2016-12-06', 25, 153, 1, 1),
-(16, '2016-11-29', '2016-12-06', 20, 209, 1, 1);
+(16, '2016-11-29', '2016-12-06', 20, 209, 1, 1),
+(17, '2016-11-29', '2016-12-06', 40, 222, 13, 1);
 
 -- --------------------------------------------------------
 
@@ -235,7 +196,7 @@ CREATE TABLE `staff` (
 INSERT INTO `staff` (`staffID`, `fName`, `lName`, `username`, `passwd`, `gender`, `jobID`) VALUES
 (1, 'Rebecca', 'Liverdale', 'rLiverdale', '123', 'female', '2'),
 (2, 'Ben', 'Affleck', 'batman', 'joker', 'Male', '1'),
-(3, 'Ra''s', 'Al Ghul', 'demon', 'lazarus', 'Male', '2');
+(3, 'Ra\'s', 'Al Ghul', 'demon', 'lazarus', 'Male', '2');
 
 -- --------------------------------------------------------
 
@@ -262,10 +223,11 @@ CREATE TABLE `students` (
 --
 
 INSERT INTO `students` (`studentID`, `fName`, `lName`, `faculty`, `email`, `username`, `passwd`, `gender`, `age`, `anyRent`, `overduePay`) VALUES
-(1, 'Mihindu', 'de Silva', 'Art', 'mihindu@gmail.com', 'mdesilva', '123', 'male', 19, 1, 0),
+(1, 'Mihindu', 'de Silva', 'Computer Engineering', 'mihindu@gmail.com', 'mdesilva', '123', 'male', 19, 1, 0),
 (3, 'Michael', 'Dawson', 'Dragons', 'mDawson@gmail.com', 'mDawson', 'ryu', 'Male', 19, 0, 0),
-(13, 'Alex', 'Dally', '', 'aDally@gmail.com', 'aDally', 'alex', 'Male', 20, 0, 0),
-(14, 'Barry', 'Allen', '', 'flash@gmail.com', 'flash', 'reverse', 'Male', 201, 1, 0);
+(13, 'Alex', 'Dally', 'Art', 'aDally@gmail.com', 'aDally', 'alex', 'Male', 20, 1, 0),
+(14, 'Barry', 'Allen', 'Speed Force', 'flash@gmail.com', 'flash', 'reverse', 'Male', 201, 1, 0),
+(15, 'Oliver', 'Queen', 'Caves', 'arrowCave@arrow.com', 'arrow', 'green', 'Male', 42, 0, 0);
 
 --
 -- Indexes for dumped tables
@@ -275,8 +237,7 @@ INSERT INTO `students` (`studentID`, `fName`, `lName`, `faculty`, `email`, `user
 -- Indexes for table `authors`
 --
 ALTER TABLE `authors`
-  ADD PRIMARY KEY (`authorID`),
-  ADD KEY `publisher` (`publisher`);
+  ADD PRIMARY KEY (`authorID`);
 
 --
 -- Indexes for table `book`
@@ -296,21 +257,6 @@ ALTER TABLE `bookcopies`
 --
 ALTER TABLE `jobs`
   ADD PRIMARY KEY (`jobID`);
-
---
--- Indexes for table `publishers`
---
-ALTER TABLE `publishers`
-  ADD PRIMARY KEY (`pubID`);
-
---
--- Indexes for table `purchaseorders`
---
-ALTER TABLE `purchaseorders`
-  ADD PRIMARY KEY (`orderID`),
-  ADD KEY `bookISBN` (`bookISBN`),
-  ADD KEY `publisher` (`publisher`),
-  ADD KEY `staffMember` (`staffMember`);
 
 --
 -- Indexes for table `rentdetails`
@@ -346,32 +292,22 @@ ALTER TABLE `authors`
 -- AUTO_INCREMENT for table `book`
 --
 ALTER TABLE `book`
-  MODIFY `bookID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `bookID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT for table `bookcopies`
 --
 ALTER TABLE `bookcopies`
-  MODIFY `bookCopyID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=247;
+  MODIFY `bookCopyID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=277;
 --
 -- AUTO_INCREMENT for table `jobs`
 --
 ALTER TABLE `jobs`
   MODIFY `jobID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
--- AUTO_INCREMENT for table `publishers`
---
-ALTER TABLE `publishers`
-  MODIFY `pubID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `purchaseorders`
---
-ALTER TABLE `purchaseorders`
-  MODIFY `orderID` int(11) NOT NULL AUTO_INCREMENT;
---
 -- AUTO_INCREMENT for table `rentdetails`
 --
 ALTER TABLE `rentdetails`
-  MODIFY `rentID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `rentID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 --
 -- AUTO_INCREMENT for table `staff`
 --
@@ -381,19 +317,4 @@ ALTER TABLE `staff`
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `studentID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `purchaseorders`
---
-ALTER TABLE `purchaseorders`
-  ADD CONSTRAINT `order_book_fk` FOREIGN KEY (`bookISBN`) REFERENCES `book` (`bookID`) ON DELETE NO ACTION ON UPDATE CASCADE,
-  ADD CONSTRAINT `order_publisher_fk` FOREIGN KEY (`publisher`) REFERENCES `publishers` (`pubID`),
-  ADD CONSTRAINT `order_staff` FOREIGN KEY (`staffMember`) REFERENCES `staff` (`staffID`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  MODIFY `studentID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
